fd722a4211325917abb75311632cc5b14bb349af
Stephen Bodey <stephen.bodey@radiancetech.com>
2025-12-26 10:24:57 -0500

    Testing basic autonomous positioning.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,11 +1,21 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
+    final static private int HOME_X = 56;
+    final static private int HOME_Y = 10;
+    final static private int HOME_ANGLE = 45;
+
+    final static private Pose2d farShootingPos = new Pose2d(HOME_X, HOME_Y, Math.toRadians(-60));
+    final static private Pose2d midShootingPos = new Pose2d(0, 0, Math.toRadians(45));
+    final static private Pose2d nearShootingPos = new Pose2d(-25, 20, Math.toRadians(45));
+    final static private Pose2d loadingPos = new Pose2d( 56, 55, Math.toRadians(45));
+
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -16,15 +26,18 @@ public class MeepMeepTesting {
 
         // The X/Y is the coordinate on the mapped field for DECODE. linToX/Y drives a straight line to
         // that coordinate
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(61, 16, 0))
-                .lineToX(-52)
-                .turn(Math.toRadians(90))
-                .lineToY(50)
-                .turn(Math.toRadians(90))
-                .lineToX(61 )
-                .turn(Math.toRadians(90))
-                .lineToY(16)
-                .turn(Math.toRadians(90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(HOME_X, HOME_Y, Math.toRadians(HOME_ANGLE)))
+                // add action to shoot initially loaded balls from far position
+                .splineTo(new Vector2d(36, 40), Math.toRadians(90))
+                .splineTo(midShootingPos.position, midShootingPos.heading.real)
+                // add action to shoot balls in color order
+                .splineTo(new Vector2d(10, 40), Math.toRadians(90))
+                .splineTo(midShootingPos.position, midShootingPos.heading.real)
+                // add action to shoot balls in color order
+                .splineTo(new Vector2d(-15, 40), Math.toRadians(90))
+                .splineTo(nearShootingPos.position, nearShootingPos.heading.real)
+                // add action to shoot balls in color order
+                .splineTo(loadingPos.position, loadingPos.heading.real)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
