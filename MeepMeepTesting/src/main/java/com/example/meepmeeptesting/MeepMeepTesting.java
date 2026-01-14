632989ffe2b4ee791d0ecbd36b0d22a26f23ef22
Stephen Bodey <stephen.bodey@radiancetech.com>
2026-01-14 12:28:41 -0500

    Reworked the trajectory pathing using MeepMeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -3,86 +3,168 @@ package com.example.meepmeeptesting;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
-    final static private int HOME_X = 61;
-    final static private int HOME_Y = -10;
-    final static private int HOME_ANGLE = -45;
+    final static private int RED_HOME_X = 61;
+    final static private int RED_HOME_Y = -10;
+    final static private int RED_HOME_ANGLE = 180;
 
     // Red Team setup
-    final static private Pose2d farShootingPos = new Pose2d(61, -10, Math.toRadians(-60));
-    final static private Pose2d midShootingPos = new Pose2d(0, 0, Math.toRadians(45));
-    final static private Pose2d nearShootingPos = new Pose2d(-15, -13.2, Math.toRadians(45));
-    final static private Pose2d loadingPos = new Pose2d( 56, -56, Math.toRadians(45));
+    final static private Pose2d red_farShootingPos = new Pose2d(51, -7, Math.toRadians(-60));
+    final static private Pose2d red_midShootingPos = new Pose2d(0, 0, Math.toRadians(45));
+    final static private Pose2d red_nearShootingPos = new Pose2d(-15, -13.2, Math.toRadians(45));
+    final static private Pose2d red_loadingPos = new Pose2d( 52, -52, Math.toRadians(45));
 
-    static private final Pose2d firstLinePos = new Pose2d(36, -42, Math.toRadians(-90));
-    static private final Pose2d secondLinePos = new Pose2d(11.6, -42, Math.toRadians(-90));
-    static private final Pose2d thirdLinePos = new Pose2d(-12, -42, Math.toRadians(-90));
+    static private final Pose2d red_firstLinePos = new Pose2d(36, -42, Math.toRadians(-90));
+    static private final Pose2d red_secondLinePos = new Pose2d(11.6, -42, Math.toRadians(-90));
+    static private final Pose2d red_thirdLinePos = new Pose2d(-12, -42, Math.toRadians(-90));
+
+    final static private int BLUE_HOME_X = 61;
+    final static private int BLUE_HOME_Y = 10;
+    final static private int BLUE_HOME_ANGLE = 180;
+
+    // Red Team setup
+    final static private Pose2d blue_farShootingPos = new Pose2d(51, 7, Math.toRadians(-60));
+    final static private Pose2d blue_midShootingPos = new Pose2d(0, 0, Math.toRadians(45));
+    final static private Pose2d blue_nearShootingPos = new Pose2d(-15, 13.2, Math.toRadians(45));
+    final static private Pose2d blue_loadingPos = new Pose2d( 52, 52, Math.toRadians(45));
+
+    static private final Pose2d blue_firstLinePos = new Pose2d(36, 42, Math.toRadians(-90));
+    static private final Pose2d blue_secondLinePos = new Pose2d(11.6, 42, Math.toRadians(-90));
+    static private final Pose2d blue_thirdLinePos = new Pose2d(-12, 42, Math.toRadians(-90));
 
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+        RoadRunnerBotEntity blueBot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be blue
+                .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(90, 75, Math.toRadians(180), Math.toRadians(180), 16)
+                .setDimensions(17, 16)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(HOME_X, HOME_Y, Math.toRadians(HOME_ANGLE)))
+        RoadRunnerBotEntity redBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(90, 75, Math.toRadians(180), Math.toRadians(180), 16)
+                .setDimensions(17, 16)
+                .build();
+
+        redBot.runAction(redBot.getDrive().actionBuilder(new Pose2d(RED_HOME_X, RED_HOME_Y, Math.toRadians(RED_HOME_ANGLE)))
                 .lineToX(56) // Pull away from wall
                 // Auto search for target QR
                 // Fire 3 into target
                 .waitSeconds(3)
                 // Turn on sweeper, set revolver to loading position
                 // ***FIRST LINE OF BALLS
-                .splineTo(new Vector2d(firstLinePos.component1().x, firstLinePos.component1().y), Math.toRadians(-90)) // ingest first ball
+                .splineTo(new Vector2d(red_firstLinePos.component1().x, red_firstLinePos.component1().y), Math.toRadians(-90)) // ingest first ball
                 // revolver to next loading position
                 .waitSeconds(1)
-                .lineToY(firstLinePos.component1().y - 5) // ingest 2nd ball
+                .lineToY(red_firstLinePos.component1().y - 5) // ingest 2nd ball
                 // revolver to next loading position
                 .waitSeconds(1)
-                .lineToY(firstLinePos.component1().y - 10) // ingest 2nd ball
-                .splineTo(midShootingPos.position, midShootingPos.heading.real)
+                .lineToY(red_firstLinePos.component1().y - 10) // ingest 2nd ball
+                .splineToConstantHeading(red_farShootingPos.position, 90)
+                .turn(Math.toRadians(60))
                 // Auto search for target QR
                 // Fire 3 into target
                 .waitSeconds(3)
 
                 // ***SECOND LINE OF BALLS
-                .splineTo(new Vector2d(secondLinePos.component1().x, secondLinePos.component1().y), Math.toRadians(-90))
+                .splineTo(new Vector2d(red_secondLinePos.component1().x, red_secondLinePos.component1().y), Math.toRadians(-90))
                 // revolver to next loading position
                 .waitSeconds(1)
-                .lineToY(secondLinePos.component1().y-5) // ingest 2nd ball
+                .lineToY(red_secondLinePos.component1().y - 5) // ingest 2nd ball
                 // revolver to next loading position
                 .waitSeconds(1)
-                .lineToY(secondLinePos.component1().y-10) // ingest 2nd ball
-                .splineTo(midShootingPos.position, midShootingPos.heading.real)
+                .lineToY(red_secondLinePos.component1().y - 10) // ingest 2nd ball
+                .splineToConstantHeading(red_midShootingPos.position, 90)
+                .turn(Math.toRadians(45))
                 // Auto search for target QR
                 // Fire 3 into target
                 .waitSeconds(3)
 
                 //***THIRD LINE OF BALLS
-                .splineTo(new Vector2d(thirdLinePos.component1().x, thirdLinePos.component1().y), Math.toRadians(-90))
+                .splineTo(new Vector2d(red_thirdLinePos.component1().x, red_thirdLinePos.component1().y), Math.toRadians(-90))
                 // revolver to next loading position
                 .waitSeconds(1)
-                .lineToY(thirdLinePos.component1().y - 5) // ingest 2nd ball
+                .lineToY(red_thirdLinePos.component1().y - 5) // ingest 2nd ball
                 // revolver to next loading position
                 .waitSeconds(1)
-                .lineToY(thirdLinePos.component1().y - 10) // ingest 2nd ball
-                .splineTo(nearShootingPos.position, nearShootingPos.heading.real)
+                .lineToY(red_thirdLinePos.component1().y - 10) // ingest 2nd ball
+                .splineToConstantHeading(red_nearShootingPos.position, -90)
+                                .turn(Math.toRadians(30))
                 // Auto search for target QR
                 // Fire 3 into target
                 .waitSeconds(3)
 
                 // add action to shoot balls in color order
-                .splineTo(loadingPos.position, loadingPos.heading.real)
+                .splineTo(red_loadingPos.position, red_loadingPos.heading.real)
+                .build()
+        );
+
+        blueBot.runAction(blueBot.getDrive().actionBuilder(new Pose2d(BLUE_HOME_X, BLUE_HOME_Y, Math.toRadians(BLUE_HOME_ANGLE)))
+                .lineToX(56) // Pull away from wall
+                // Auto search for target QR
+                // Fire 3 into target
+                .waitSeconds(3)
+                // Turn on sweeper, set revolver to loading position
+                // ***FIRST LINE OF BALLS
+                .splineTo(new Vector2d(blue_firstLinePos.component1().x, blue_firstLinePos.component1().y), Math.toRadians(90)) // ingest first ball
+                // revolver to next loading position
+                .waitSeconds(1)
+                .lineToY(blue_firstLinePos.component1().y + 5) // ingest 2nd ball
+                // revolver to next loading position
+                .waitSeconds(1)
+                .lineToY(blue_firstLinePos.component1().y + 10) // ingest 2nd ball
+                .splineToConstantHeading(blue_farShootingPos.position, -90)
+                .turn(Math.toRadians(-60))
+                // Auto search for target QR
+                // Fire 3 into target
+                .waitSeconds(3)
+
+                // ***SECOND LINE OF BALLS
+                .splineTo(new Vector2d(blue_secondLinePos.component1().x, blue_secondLinePos.component1().y), Math.toRadians(90))
+                // revolver to next loading position
+                .waitSeconds(1)
+                .lineToY(blue_secondLinePos.component1().y + 5) // ingest 2nd ball
+                // revolver to next loading position
+                .waitSeconds(1)
+                .lineToY(blue_secondLinePos.component1().y + 10) // ingest 2nd ball
+                .splineToConstantHeading(blue_midShootingPos.position, -90)
+                .turn(Math.toRadians(-45))
+                // Auto search for target QR
+                // Fire 3 into target
+                .waitSeconds(3)
+
+                //***THIRD LINE OF BALLS
+                .splineTo(new Vector2d(blue_thirdLinePos.component1().x, blue_thirdLinePos.component1().y), Math.toRadians(90))
+                // revolver to next loading position
+                .waitSeconds(1)
+                .lineToY(blue_thirdLinePos.component1().y + 5) // ingest 2nd ball
+                // revolver to next loading position
+                .waitSeconds(1)
+                .lineToY(blue_thirdLinePos.component1().y + 10) // ingest 2nd ball
+                .splineToConstantHeading(blue_nearShootingPos.position, 90)
+                .turn(Math.toRadians(-30))
+                // Auto search for target QR
+                // Fire 3 into target
+                .waitSeconds(3)
+
+                // add action to shoot balls in color order
+                .splineTo(blue_loadingPos.position, blue_loadingPos.heading.real)
                 .build()
         );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(blueBot)
+                .addEntity(redBot)
                 .start();
     }
 }
